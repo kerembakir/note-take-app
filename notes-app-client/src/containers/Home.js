7ed746b7c71d66391e3616a73e7a823ce07c8017
kerembakir <kerembakir@gmail.com>
2017-07-03 21:48:38 +0200

    Added Routes to render

---
+++ notes-app-client/src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Note App</h1>
+          <p>A Note taking app</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;

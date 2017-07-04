bf6a3640bf503b9b022bb4c3c5c30621b69aff4a
kerembakir <kerembakir@gmail.com>
2017-07-04 18:06:31 +0200

    Save Changes to a Note

--- notes-app-client/src/containers/Notes.js
+++ notes-app-client/src/containers/Notes.js
@@ -6,7 +6,7 @@ import {
   ControlLabel,
 } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
-import { invokeApig } from '../libs/awsLib';
+import { invokeApig, s3Upload } from '../libs/awsLib';
 import config from '../config.js';
 import './Notes.css';
 
@@ -62,7 +62,17 @@ class Notes extends Component {
     this.file = event.target.files[0];
   }
 
+  saveNote(note) {
+    return invokeApig({
+      path: `/notes/${this.props.match.params.id}`,
+      method: 'PUT',
+      body: note,
+    }, this.props.userToken);
+  }
+
   handleSubmit = async (event) => {
+    let uploadedFilename;
+
     event.preventDefault();
 
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
@@ -71,6 +81,24 @@ class Notes extends Component {
     }
 
     this.setState({ isLoading: true });
+
+    try {
+
+      if (this.file) {
+        uploadedFilename = (await s3Upload(this.file, this.props.userToken)).Location;
+      }
+
+      await this.saveNote({
+        ...this.state.note,
+        content: this.state.content,
+        attachment: uploadedFilename || this.state.note.attachment,
+      });
+      this.props.history.push('/');
+    }
+    catch(e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
   }
 
   handleDelete = async (event) => {

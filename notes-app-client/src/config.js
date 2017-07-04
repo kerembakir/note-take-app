7ec7cdb8ea61ed105360488320dc1c804d6de2b0
kerembakir <kerembakir@gmail.com>
2017-07-04 16:13:09 +0200

    Call the Create API, Calling API Gateway, create a helper function

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,5 +1,8 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://cv0m56evo9.execute-api.us-east-1.amazonaws.com/prod/',
+  },
   cognito: {
     USER_POOL_ID : 'us-east-2_93RI4qz0R',
     APP_CLIENT_ID : '382cel3tbn6mpg2kk3mmhaib0b',

f6f93d223e9a8491566675ccad70d199d6fc46bf
kerembakir <kerembakir@gmail.com>
2017-07-04 16:27:16 +0200

    Get Cognito Identity Pool Credentials

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -6,5 +6,7 @@ export default {
   cognito: {
     USER_POOL_ID : 'us-east-2_93RI4qz0R',
     APP_CLIENT_ID : '382cel3tbn6mpg2kk3mmhaib0b',
+    REGION: 'us-east-2',
+    IDENTITY_POOL_ID: 'us-east-2:78ff4560-5fee-11e7-85f8-d57bdb0f0db2',
   }
 };
--- notes-app-client/src/libs/awsLib.js
+++ notes-app-client/src/libs/awsLib.js
@@ -1,3 +1,4 @@
+import AWS from 'aws-sdk';
 import config from '../config.js';
 
 export async function invokeApig(
@@ -23,4 +24,19 @@ export async function invokeApig(
   }
 
   return results.json();
+}
+
+export function getAwsCredentials(userToken) {
+  const authenticator = `cognito-idp.${config.cognito.REGION}.amazonaws.com/${config.cognito.USER_POOL_ID}`;
+
+  AWS.config.update({ region: config.cognito.REGION });
+
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.cognito.IDENTITY_POOL_ID,
+    Logins: {
+      [authenticator]: userToken
+    }
+  });
+
+  return AWS.config.credentials.getPromise();
 }
